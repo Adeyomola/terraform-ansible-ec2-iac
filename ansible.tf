0e457ad6bf60aef9569c49defd9f0c9fb6cfd47b
Adeyomola <kayzeel@yahoo.com>
2023-01-18 20:27:22 +0100

    Created directory for ansible files

--- ansible.tf
+++ ansible.tf
@@ -1,7 +1,7 @@
 resource "local_file" "host-inventory" {
   content  = "${aws_instance.triserver.0.public_dns}\n${aws_instance.triserver.1.public_dns}\n${aws_instance.triserver.2.public_dns}"
-  filename = "${path.cwd}/host-inventory"
+  filename = "${path.cwd}/ansible/host-inventory"
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.cwd}/host-inventory ${path.cwd}/playbook.yml"
+    command = "ansible-playbook -i ${path.cwd}/ansible/host-inventory ${path.cwd}/ansible/playbook.yml"
   }
 }
