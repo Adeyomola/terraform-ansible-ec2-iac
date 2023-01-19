c4f77d7dd26ba07b591bca688ef77b8c11d17670
Adeyomola <kayzeel@yahoo.com>
2023-01-19 20:22:07 +0100

    Commented out namecheap configurations

--- aws_terraform.tf
+++ aws_terraform.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-    namecheap = {
-      source  = "namecheap/namecheap"
-      version = ">= 2.0.0"
-    }
+    #    namecheap = {
+    #      source  = "namecheap/namecheap"
+    #      version = ">= 2.0.0"
+    #    }
   }
 }
 
@@ -17,9 +17,9 @@ provider "aws" {
 }
 
 # Namecheap API credentials
-provider "namecheap" {
-  user_name   = var.namecheap_username
-  api_user    = var.namecheap_api_user
-  api_key     = var.namecheap_api_key
-  use_sandbox = false
-}
+#provider "namecheap" {
+#  user_name   = var.namecheap_username
+#  api_user    = var.namecheap_api_user
+#  api_key     = var.namecheap_api_key
+#  use_sandbox = false
+#}
